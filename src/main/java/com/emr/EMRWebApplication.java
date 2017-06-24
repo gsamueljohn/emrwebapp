14aeb0093481cbe65d3fbcefde7fa34d28ab37be
gsamueljohn <gsamueljohn@gmail.com>
2017-06-24 00:32:01 -0700

    no message

--- src/main/java/com/emr/EMRWebApplication.java
+++ src/main/java/com/emr/EMRWebApplication.java
@@ -1,6 +1,6 @@
 package com.emr;
 
-import com.db4o.types.Blob;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
